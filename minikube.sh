ed28cd61ee577316bf23dcbc93af98407b6d243e
Enrico Sasdelli <enrico.sasdelli@motork.io>
2025-09-02 18:01:01 +0200

    otel javaagent

--- minikube.sh
+++ minikube.sh
@@ -3,4 +3,4 @@
 colima start --network-address
 minikube start
 minikube mount $(pwd)/build/libs:/apps &
-minikube mount $(pwd)/grafana:/grafana --uid 472 --gid 472 &
+minikube mount $(pwd)/data/grafana:/grafana --uid 472 --gid 472 &
