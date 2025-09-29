bc1d02388934be916c2c8c99b486b0648a8f9829
Enrico Sasdelli <enrico.sasdelli@motork.io>
2025-09-29 09:14:23 +0200

    exception events

--- app/clone-factory/src/main/java/devfest2025/CloneFactoryService.java
+++ app/clone-factory/src/main/java/devfest2025/CloneFactoryService.java
@@ -24,6 +24,10 @@ public class CloneFactoryService {
             clone.setName(RandomStringUtils.randomAlphabetic(10));
             clone.setPlanet("Coruscant");
 
+            if (Math.random() < 0.3) {
+                throw new RuntimeException("Clone creation failed");
+            }
+
             log.info("New clone is now ready");
             Clone savedClone = cloneRepository.save(clone);
             return savedClone;
